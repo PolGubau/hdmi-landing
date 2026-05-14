d64de25acdaa3025536b930c70b731a9115a7f6b
Pol Gubau Amores <gubaupol@gmail.com>
2026-05-14 08:43:09 +0200

    Remove newsletter subscription logic and related components
    This commit eliminates the newsletter subscription functionality from the codebase. The `subscribeNewsletter` action has been removed, along with the `NewsletterInput` component that handled user input for email subscriptions. Additionally, references to Supabase environment variables have been deleted, reflecting the removal of the associated database interactions. The blog index page has also been updated to remove the newsletter input component.
    
    Issue: MW-0000
    Type: refactor

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,6 @@
 interface ImportMetaEnv {
 	readonly RESEND_API_KEY: string;
 	readonly PUBLIC_GOOGLE_SHEETS_URL: string;
-	readonly PUBLIC_SUPABASE_URL: string;
-	readonly PUBLIC_SUPABASE_ANON_KEY: string;
 	readonly PUBLIC_GA_MEASUREMENT_ID: string;
 	readonly NOTION_INTEGRATION_SECRET: string;
 	readonly NOTION_DATABASE_ID: string;
